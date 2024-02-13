41eb99f113a2d67059f76629bd1b60d312d71f04
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-02-13 11:18:19 -0600

    Created IProductRepository interface

--- ORM-Dapper/Product.cs
+++ ORM-Dapper/Product.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ORM_Dapper
 {
-    internal class Product
+    public class Product
     {
         public int productID {  get; set; }
         public string Name { get; set; }
